c8b6a1b11ce3f3e87e103a0eec56667a96a643c5
machado <jmachado.gui@gmail.com>
2022-01-12 22:03:37 -0300

    Stack Navigator implementado

--- App.js
+++ App.js
@@ -8,15 +8,19 @@ import Intro from './src/pages/Intro';
 import Login from './src/pages/Login';
 import Contatos from './src/pages/Contatos';
 import Contato from './src/pages/Contato';
-
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
 const App = () => {
   return (
-    <>
-      <Intro></Intro>
-      <Login></Login>
-      <Contatos></Contatos>
-      <Contato></Contato>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Introdução' component={Intro}/>
+        <Stack.Screen name='Login' component={Login}/>
+        <Stack.Screen name='Contatos' component={Contatos}/>
+        <Stack.Screen name='Contato' component={Contato}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
