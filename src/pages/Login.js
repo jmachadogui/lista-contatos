2192b95d48ee4f5b6bba6408f9f1db67a0642cc3
machado <jmachado.gui@gmail.com>
2022-01-13 22:34:38 -0300

    Testa requests: GET, POST, PUT, DELETE

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,7 +9,7 @@ import { clearWarnings } from 'react-native/Libraries/LogBox/Data/LogBoxData';
 const axios = require('axios')
 const base_url = 'https://contacts-api.prd.parceirodaconstrucao.com.br';
 
-async function testePost(){
+async function testLogin(){
     let url = base_url+'/auth/login';
     console.log('URL', url)
     let data = {
@@ -19,15 +19,99 @@ async function testePost(){
 
     let response = await axios.post(url, data)
     console.log('RESPONSE POST', response.data)
+    console.log('TOKEN', response.data.token)
+    AsyncStorageLib.setItem('TOKEN', response.data.token)
+}
+async function testeGet(){
+  let url = base_url+'/contacts';
+  console.log('GET', url)
+  let token =await AsyncStorageLib.getItem('TOKEN');
+  console.log('GET TOKEN', token)
+  let response = await axios({
+    url: url,
+    method: "GET",
+    headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + token
+    }
+  })
+  console.log(response.data)
+}
+async function testePost(){
+  let url = base_url+'/contacts';
+  console.log('POST', url)
+  let token =await AsyncStorageLib.getItem('TOKEN');
+  
+  let data = {
+    name: "Guilherme Machado",
+    mobile: "51997695256",
+    email: "jmachado.gui@gmail.com"
+  }
+
+  let response = await axios({
+    url: url,
+    method: "POST",
+    data:data,
+    headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + token
+    }
+  })
+  console.log(response.data)
+}
+async function testePut(){
+  let url = base_url+'/contacts/16';
+  console.log('PUT', url)
+  let token =await AsyncStorageLib.getItem('TOKEN');
+  
+  let data = {
+    name: "Machado",
+    mobile: "51997695256",
+    email: "jmachado.gui@gmail.com"
+  }
+
+  let response = await axios({
+    url: url,
+    method: "PUT",
+    data:data,
+    headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + token
+    }
+  })
+  console.log(response.data)
+}
+async function testeDelete(){
+  let url = base_url+'/contacts/16';
+  console.log('DELETE', url)
+  let token =await AsyncStorageLib.getItem('TOKEN');
+
+  let response = await axios({
+    url: url,
+    method: "DELETE",
+    headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + token
+    }
+  })
+  console.log(response.data)
 }
 const Login = ({navigation}) => {
   return (
     <>
       <Button title='Entrar' onPress={()=>navigation.navigate('Contatos')}>   
       </Button>
-     
+      <Button title='Teste Login' onPress={()=>testLogin()}>   
+      </Button>
+      <Button title='Teste GET' onPress={()=>testeGet()}>   
+      </Button>
       <Button title='Teste POST' onPress={()=>testePost()}>   
       </Button>
+      <Button title='Teste PUT' onPress={()=>testePut()}>   
+      </Button>
+      <Button title='Teste DELETE' onPress={()=>testeDelete()}>   
+      </Button>
+     
     </>
   );
 };
