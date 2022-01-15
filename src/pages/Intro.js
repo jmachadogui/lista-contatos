7839f8330b6af4c100c0ae88747f3ae300be2136
machado <jmachado.gui@gmail.com>
2022-01-15 14:29:37 -0300

    Testa styled components

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -5,11 +5,13 @@ import {
   Text,
 } from 'react-native';
 import { ArrowUpCircle } from "react-native-feather";
+import { Title } from '../atoms/styles';
 const Intro = ({navigation}) => {
   return (
     <>
       <Button title='Ir para Login' onPress={()=>navigation.navigate('Login')}>   
       </Button>
+      <Title>Titulo</Title>
       <Text style={{fontFamily: 'Montserrat-Black', color:'black'}}>Texto teste</Text>
     </>
   );
