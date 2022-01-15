dbdc318b08197c0b0ef4234eb89ba63a08f88b8e
machado <jmachado.gui@gmail.com>
2022-01-15 11:16:59 -0300

    Instala fonte Montserrat

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -4,12 +4,13 @@ import {
     Button,
   Text,
 } from 'react-native';
-
+import { ArrowUpCircle } from "react-native-feather";
 const Intro = ({navigation}) => {
   return (
     <>
       <Button title='Ir para Login' onPress={()=>navigation.navigate('Login')}>   
       </Button>
+      <Text style={{fontFamily: 'Montserrat-Black', color:'black'}}>Texto teste</Text>
     </>
   );
 };
