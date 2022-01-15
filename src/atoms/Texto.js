917fb8b51246f3d26cb0724dc6c2aae622e110f6
machado <jmachado.gui@gmail.com>
2022-01-15 17:33:50 -0300

    Adiciona componente Texto

---
+++ src/atoms/Texto.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from 'styled-components/native';
+
+const Texto = styled.Text`
+    fontFamily: Montserrat-Regular;
+    color: ${props=>props.color != null? props.color:'#757575'};
+    fontSize: ${props=>props.fontSize != null? props.fontSize:'16px'};
+    width: ${props=> props.width != null? props.width:'100%'};
+    textAlign:${props=> props.textAlign != null? props.textAlign:'left'};
+`
+export default Texto;
--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -8,6 +8,7 @@ import { ArrowUpCircle } from "react-native-feather";
 import If from '../atoms/If';
 import { Title } from '../atoms/styles';
 import H1 from '../atoms/H1';
+import Texto from '../atoms/Texto';
 const Intro = ({navigation}) => {
   return (
     <>
@@ -15,6 +16,7 @@ const Intro = ({navigation}) => {
         <Text style={{color:'red'}}>a</Text>
       </If>
       <H1>Aaaaaa</H1>
+      <Texto>Faça login para acessar nossa plataforma</Texto>
     </>
   );
 };
