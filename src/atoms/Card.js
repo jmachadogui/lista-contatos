ef5e88c61c3a30a6970eafdc7912603227e558d6
machado <jmachado.gui@gmail.com>
2022-01-16 16:43:20 -0300

    BugFix: Adiciona unidade de medida para border radius

--- src/atoms/Card.js
+++ src/atoms/Card.js
@@ -4,7 +4,7 @@ const Card = styled.View`
     background-color: #FFFFFF,
     borderWidth: 1px;
     borderColor: #E0E0E0;
-    borderRadius: 8;
+    borderRadius: 8px;
     flex: ${props => props.flex != null? props.flex:'none'};
     padding:16px;
 `
