a19a02655b912876af03b3aaf2b7e8f003d5540d
machado <jmachado.gui@gmail.com>
2022-01-16 20:59:47 -0300

    Implementa inputs para cadastro de contato

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -1,14 +1,34 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Text,
 } from 'react-native';
-
+import Container from '../atoms/Container';
+import Texto from '../atoms/Texto';
+import Botao from '../molecules/Botao';
+import Input from '../molecules/Input'
 const Contato = () => {
+  const [contato, setContato] = useState({
+    name:'',
+    email:'',
+    mobile:''
+  })
+  const handleChange = (name, value) => {
+    let state = contato;
+    state[name] = value;
+    setContato(state);
+  }
+  function salvar(){
+    console.log(contato);
+  }
   return (
-    <>
-      <Text>Contato</Text>
-    </>
+    <Container justifyContent='flex-start' alignItems='center'>
+      <Texto textAlign='center' width='80%'>Preencha as informações para cadastrar um novo contato</Texto>
+      <Input name='name' label='Nome Completo' value={contato.name} onChange={(name, value) => handleChange(name, value)}/>
+      <Input name='email' label='Email' value={contato.email} onChange={(name, value) => handleChange(name, value)}/>
+      <Input name='mobile' label='Celular' keyboardType='numeric' value={contato.mobile} onChange={(name, value) => handleChange(name, value)}/>
+      <Botao title='Cadastrar Contato' onPress={()=>salvar()}/>
+    </Container>
   );
 };
 
