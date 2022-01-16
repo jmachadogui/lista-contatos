73ef05ae4ce87034d172076daa827c61e79f928e
machado <jmachado.gui@gmail.com>
2022-01-16 15:57:02 -0300

    Ajustado para exibir informações de contato via props

--- src/organisms/ItemContato.js
+++ src/organisms/ItemContato.js
@@ -5,9 +5,11 @@ import Texto from "../atoms/Texto";
 
 const ItemContato = (props) => {
     return (
-        <View style={{width:'100%', height:70}}>
+        <View style={{width:'100%', height:90}}>
          <Card flex={1}>
-             <Texto>teste</Texto>
+             <Texto>{props.contato.name}</Texto>
+             <Texto>{props.contato.email}</Texto>
+             <Texto>{props.contato.mobile}</Texto>
          </Card>
         </View>
     )
