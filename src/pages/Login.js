02e9f4d6fa9e923ecd87fe3c3ed2ffe5b108327f
machado <jmachado.gui@gmail.com>
2022-01-16 15:12:00 -0300

    Implementada estrutura inicial página Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,118 +1,51 @@
 
 import AsyncStorageLib from '@react-native-async-storage/async-storage';
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
   Text,
 } from 'react-native';
 import { clearWarnings } from 'react-native/Libraries/LogBox/Data/LogBoxData';
+import Container from '../atoms/Container';
+import H1 from '../atoms/H1';
+import Texto from '../atoms/Texto';
+import Botao from '../molecules/Botao';
+import Input from '../molecules/Input';
+import api from '../util/api';
+import Config from '../util/Config';
 const axios = require('axios')
 const base_url = 'https://contacts-api.prd.parceirodaconstrucao.com.br';
 
-async function testLogin(){
-    let url = base_url+'/auth/login';
-    console.log('URL', url)
-    let data = {
-      email: 'user@diwe.com.br',
-      password: 'Mob20we23##'
-    }
-
-    let response = await axios.post(url, data)
-    console.log('RESPONSE POST', response.data)
-    console.log('TOKEN', response.data.token)
-    AsyncStorageLib.setItem('TOKEN', response.data.token)
-}
-async function testeGet(){
-  let url = base_url+'/contacts';
-  console.log('GET', url)
-  let token =await AsyncStorageLib.getItem('TOKEN');
-  console.log('GET TOKEN', token)
-  let response = await axios({
-    url: url,
-    method: "GET",
-    headers: {
-        Accept: 'application/json',
-        Authorization: 'Bearer ' + token
-    }
-  })
-  console.log(response.data)
-}
-async function testePost(){
-  let url = base_url+'/contacts';
-  console.log('POST', url)
-  let token =await AsyncStorageLib.getItem('TOKEN');
-  
-  let data = {
-    name: "Guilherme Machado",
-    mobile: "51997695256",
-    email: "jmachado.gui@gmail.com"
-  }
-
-  let response = await axios({
-    url: url,
-    method: "POST",
-    data:data,
-    headers: {
-        Accept: 'application/json',
-        Authorization: 'Bearer ' + token
-    }
-  })
-  console.log(response.data)
-}
-async function testePut(){
-  let url = base_url+'/contacts/16';
-  console.log('PUT', url)
-  let token =await AsyncStorageLib.getItem('TOKEN');
-  
-  let data = {
-    name: "Machado",
-    mobile: "51997695256",
-    email: "jmachado.gui@gmail.com"
-  }
-
-  let response = await axios({
-    url: url,
-    method: "PUT",
-    data:data,
-    headers: {
-        Accept: 'application/json',
-        Authorization: 'Bearer ' + token
-    }
-  })
-  console.log(response.data)
-}
-async function testeDelete(){
-  let url = base_url+'/contacts/16';
-  console.log('DELETE', url)
-  let token =await AsyncStorageLib.getItem('TOKEN');
-
-  let response = await axios({
-    url: url,
-    method: "DELETE",
-    headers: {
-        Accept: 'application/json',
-        Authorization: 'Bearer ' + token
-    }
-  })
-  console.log(response.data)
-}
 const Login = ({navigation}) => {
+  const [infoLogin, setInfoLogin] = useState({email:'user@diwe.com.br', password:'Mob20we23##'})
+  const handleChange = (name, value) => {
+    console.log(name, value);
+    let state = infoLogin;
+    state[name] = value;
+    setInfoLogin(state)
+  }
+  async function logar(){
+      let response = await api.logar(infoLogin)
+      if(response.error){
+        alert('Credenciais inválidas. Por favor, tente novamente');
+      } else {
+        navigation.navigate('Contatos')
+      }
+  }
+  async function get(){
+     let response = await api.request('GET', '/contacts');
+     console.log(response.data);
+  }
   return (
-    <>
-      <Button title='Entrar' onPress={()=>navigation.navigate('Contatos')}>   
-      </Button>
-      <Button title='Teste Login' onPress={()=>testLogin()}>   
-      </Button>
-      <Button title='Teste GET' onPress={()=>testeGet()}>   
-      </Button>
-      <Button title='Teste POST' onPress={()=>testePost()}>   
-      </Button>
-      <Button title='Teste PUT' onPress={()=>testePut()}>   
-      </Button>
-      <Button title='Teste DELETE' onPress={()=>testeDelete()}>   
-      </Button>
-     
-    </>
+    <Container justifyContent='flex-start' alignItems='center'>
+      <H1>Bem Vindo(a)!</H1>
+      <Texto width='80%' textAlign='center'>Faça login para acessar nossa plataforma</Texto>
+      <Input placeholder='Digite seu email' label='Email' name='email' value={infoLogin.email} onChange={(name, value)=>handleChange(name, value)}/>
+      <Input placeholder='Digite sua senha' label='Senha' name='password' value={infoLogin.password} onChange={(name, value)=>handleChange(name, value)}/>
+      <Botao title='Entrar' onPress={()=>logar()}/>
+      <Botao title='GET' onPress={()=>get()}/>
+    
+    </Container>
   );
 };
 
