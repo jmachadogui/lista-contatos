718be019b7b1d8d60e0288a88380660a07a479b4
machado <jmachado.gui@gmail.com>
2022-01-16 16:08:16 -0300

    Implementado método para carregamento de contatos

--- src/templates/ListaContatos.js
+++ src/templates/ListaContatos.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "../atoms/Container";
 import Texto from "../atoms/Texto";
 import ItemContato from "../organisms/ItemContato";
-
+import api from '../util/api'
 const ListaContatos = (props) => {
+    const [contatos, setContatos] = useState([]);
+    useEffect(() => {
+        async function carregarContatos(){
+            let response = await api.request('GET', '/contacts');
+            console.log('CONTATOS', response);
+            setContatos(response)
+        }
+        carregarContatos()
+    },[])
     return (
         <Container justifyContent='flex-start'>
-           <ItemContato></ItemContato>
+           
         </Container>
     )
 }
