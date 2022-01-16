f25ec29b8b7941281e3c9326c74c467ab6c6804b
machado <jmachado.gui@gmail.com>
2022-01-15 22:30:14 -0300

    Aplica fontWeight no componente Texto

--- src/atoms/Texto.js
+++ src/atoms/Texto.js
@@ -6,6 +6,7 @@ const Texto = styled.Text`
     color: ${props=>props.color != null? props.color:'#757575'};
     fontSize: ${props=>props.fontSize != null? props.fontSize:'16px'};
     width: ${props=> props.width != null? props.width:'100%'};
+    fontWeight: ${props=> props.fontWeight != null? props.fontWeight:'500'};
     textAlign:${props=> props.textAlign != null? props.textAlign:'left'};
 `
 export default Texto;
