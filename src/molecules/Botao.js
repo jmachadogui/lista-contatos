560c8eba2e6f233589ee09eee7ae62e1966d3cef
machado <jmachado.gui@gmail.com>
2022-01-16 09:24:17 -0300

    Cria componente Botao

---
+++ src/molecules/Botao.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { TouchableOpacity } from "react-native";
+import Texto from "../atoms/Texto";
+
+const Botao = (props) => {
+    return (
+        <TouchableOpacity
+            style={{
+                height:56,
+                paddingHorizontal:16,
+                paddingVertical:16,
+                backgroundColor:'#142B5D',
+                flexDirection:'row',
+                // flex:1,
+                // width:'50%',
+                alignItems:'center'
+            }}
+            onPress={()=>props.onPress()}
+        >
+            <Texto color='#F1F1F1' textAlign='center'>{props.title}</Texto>
+        </TouchableOpacity>
+    )
+} 
+export default Botao;
