3107a725a00ed01ab1cf3f6fd8fa0300c3dc60fd
machado <jmachado.gui@gmail.com>
2022-01-17 16:43:12 -0300

    Implementada função de formatação de texto no input (telefone)

--- src/molecules/Input.js
+++ src/molecules/Input.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { TextInput } from "react-native";
 import styled from "styled-components";
 import Texto from "../atoms/Texto";
+import FormatUtil from '../util/FormatUtil';
 const EntradaTexto = styled.TextInput`
     backgroundColor:#F1F3F5;
     width: 100%;
@@ -9,17 +10,46 @@ const EntradaTexto = styled.TextInput`
     height: 56px;
     borderRadius: 8px;
 `
-const Input = (props) => {
+function Input  (props)  {
     console.log('PROPS', props.value);
     const [value, setValue] = useState(props.value);
     useEffect(()=>{
-        setValue(props.value)
+        if(!FormatUtil.isEmpty(props.value)){
+            setValue(props.value)
+        }
     },[props.value])
+
+    function handleChange(valor) {
+        console.log('HANDLE CHANGE', valor);
+        if(props.format == 'telefone'){
+            valor = formatar(valor);
+        }
+        setValue(valor);
+        console.log('VALOR', valor);
+        let valorSemFormatacao = desformatar(valor);
+        if(props.onChange != null){
+            props.onChange(props.name, valorSemFormatacao)
+
+        }
+    }
+    function formatar(v) {
+        if(props.format == 'telefone')
+            return FormatUtil.formatTelefone(v)
+        return v;
+    }
+    function desformatar (value) {
+        var valorSemFormatacao = value;
+        let {format} = props;
+        if (format === 'telefone') {
+            valorSemFormatacao = valorSemFormatacao.replace(/\D/g, "");
+        }
+        return valorSemFormatacao;
+    }
     return (
         <>
             <Texto fontWeight={500} color='#142B5D'>{props.label}</Texto>
             <EntradaTexto 
-                onChangeText={(value) => {setValue(value);props.onChange(props.name, value)}}
+                onChangeText={(value) => {handleChange(value)}}
                 value={value}
                 placeholder={props.placeholder} 
                 placeholderTextColor='#495057' 
