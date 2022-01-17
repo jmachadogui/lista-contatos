c5eb4f6062a4d3e61bb5755fddbc8b5ab31de32a
machado <jmachado.gui@gmail.com>
2022-01-17 15:51:05 -0300

    Adiciona formatação de telefone

--- src/util/FormatUtil.js
+++ src/util/FormatUtil.js
@@ -4,6 +4,20 @@ const FormatUtil = {
             return true
         } 
         return false
+    },
+    formatTelefone(v) {
+        v = '' + v;
+        v = v.replace(/\D/g, "");//Remove tudo o que não é dígito
+        if (v.length <= 9) {
+            v = v.replace(/(\d)(\d{4})$/, "$1-$2");//Coloca hífen entre o quarto e o quinto dígitos
+        } else if (v.length <= 11) {
+            v = v.replace(/^(\d{2})(\d)/g, "($1) $2");//Coloca parênteses em volta dos dois primeiros dígitos
+            v = v.replace(/(\d)(\d{4})$/, "$1-$2");//Coloca hífen entre o quarto e o quinto dígitos
+        } else {
+            v = v.replace(/^(\d{2})(\d{2})/g, "$1 ($2) ");//Coloca parênteses em volta do segundo par de dígitos
+            v = v.replace(/(\d)(\d{4})$/, "$1-$2");//Coloca hífen entre o quarto e o quinto dígitos
+        }
+        return v;
     }
 }
 
