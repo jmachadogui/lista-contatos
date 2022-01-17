388d6ee51bbdb3638bfffaf9e1dfc53d638a619d
machado <jmachado.gui@gmail.com>
2022-01-17 00:52:20 -0300

    Implementado layout final da tela de Contatos

--- src/templates/ListaContatos.js
+++ src/templates/ListaContatos.js
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import React, { useEffect, useState } from "react";
-import { FlatList, RefreshControl, View } from "react-native";
+import { FlatList, RefreshControl, Touchable, TouchableOpacity, View } from "react-native";
 import Container from "../atoms/Container";
 import Texto from "../atoms/Texto";
 import Botao from "../molecules/Botao";
@@ -31,24 +31,33 @@ const ListaContatos = (props) => {
             if(c.id == contato.id)
                 index=idx;
         })
-        let response = await api.request('DELETE', '/contacts/'+contato.id);
+        await api.request('DELETE', '/contacts/'+contato.id);
         state.splice(index, 1);
         setSelectedId(contato.id);
         setContatos(state);
     }
     return (
         <Container justifyContent='flex-start'>
-          <Botao title='Cadastrar Contato' onPress={()=>navigation.navigate('Contato')}/>
-           <FlatList 
-            extraData={selectedId}
-            data={contatos}
-            renderItem={(contato) => (
-                <View style={{marginBottom:16}}>
-                    <ItemContato contato={contato.item} 
-                        onEdit={(id) =>navigation.navigate('Contato', {id: id})}
-                        onDelete={(contato) => deleteContato(contato)}
-                    />
-                </View>)
+            <Botao marginTop={36} marginBottom={50} title='Cadastrar Contato' onPress={()=>navigation.navigate('Contato')}/>
+            <View style={{flexDirection:'row', justifyContent:'space-between', marginBottom:16}}>
+                <View style={{flex:1}}>
+                    <Texto color='#757575'>Total: {contatos.length} usuários</Texto>
+                </View>
+                <TouchableOpacity>
+                    <Texto color='#142B5D' fontSize='14px'>Ver Todos</Texto>
+                </TouchableOpacity>
+            </View>
+            <FlatList 
+                extraData={selectedId}
+                data={contatos}
+                renderItem={(contato) => (
+                    <View style={{marginBottom:16}}>
+                        <ItemContato contato={contato.item} 
+                            onEdit={(id) =>navigation.navigate('Contato', {id: id})}
+                            onDelete={(contato) => deleteContato(contato)}
+                        />
+                    </View>
+                )
             }
             keyExtractor={contato => contato.id}
             refreshControl={<RefreshControl refreshing={isRefreshing} onRefresh={onRefresh}/>}
