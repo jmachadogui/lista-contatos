885594a00d35df906125d2b15036effdd8164c07
machado <jmachado.gui@gmail.com>
2022-01-17 23:35:00 -0300

    Implementa header customizado

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 
 import React from 'react';
 import {
-  Text, View,
+  Image,
+  Text, Touchable, TouchableOpacity, View,
 } from 'react-native';
+import {ChevronLeft} from 'react-native-feather';
 import Intro from './src/pages/Intro';
 import Login from './src/pages/Login';
 import Contatos from './src/pages/Contatos';
@@ -11,7 +13,9 @@ import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Toast, { BaseToast, ErrorToast } from 'react-native-toast-message';
 import ToastCadastro from './src/molecules/ToasCadastro';
+import Header from './src/organisms/Header';
 const Stack = createNativeStackNavigator();
+import { getHeaderTitle } from '@react-navigation/elements';
 const ToastConfig = {
   success: (props) => (
       <BaseToast
@@ -33,7 +37,13 @@ const App = () => {
   return (
     <>
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{header: ({ navigation, route, options, back })=>{
+         const title = getHeaderTitle(options, route.name);
+        return <Header
+          leftButton={back? <TouchableOpacity style={{padding:10}}onPress={()=>navigation.goBack()}><ChevronLeft stroke='black'/></TouchableOpacity>:null}
+          title={title}
+        />
+      }}}>
         <Stack.Screen name='Intro' component={Intro} options={{headerShown: false}}/>
         <Stack.Screen name='Login' component={Login}/>
         <Stack.Screen name='Contatos' component={Contatos}/>
