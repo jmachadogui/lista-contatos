013fe68f4720ceb9656cad2fffab97e1f83a844c
machado <jmachado.gui@gmail.com>
2022-01-17 22:06:37 -0300

    Implementado layout final da tela Intro

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -16,15 +16,15 @@ import Container from '../atoms/Container';
 import Input from '../molecules/Input';
 import Botao from '../molecules/Botao';
 const Intro = ({navigation}) => {
-  const [valorInput, setValorInput] = useState('teste')
 
   return (
     <Container alignItems='center' justifyContent='flex-start' paddingTop={70}>
-      
-        <Image style={{marginBottom:69}} source={require('../assets/images/img_tela_intro.png')}/>
-        <H1>Bem-vindo! É hora de começar uma nova experiência</H1>
-        <Texto textAlign='center'>Para ter acesso a todas as funcionalidades que podemos oferecer, faça login ou crie uma nova conta.</Texto>
-        <Botao title='Começar' onPress={()=>navigation.navigate('Login')}/>
+          <Image style={{marginBottom:49}} source={require('../assets/images/img_tela_intro.png')}/>
+          <H1>Bem-vindo! É hora de começar uma nova experiência</H1>
+          <Texto marginTop='24px' textAlign='center'>Para ter acesso a todas as funcionalidades que podemos oferecer, faça login ou crie uma nova conta.</Texto>
+        <View style={{marginTop:40}}>
+          <Botao title='Começar' onPress={()=>navigation.navigate('Login')}/>
+        </View>
     </Container>
   );
 };
